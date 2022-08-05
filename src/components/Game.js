c86fbabf324b828016c395b08458a25cc36eb80d
Roy Kim <31496555+roy-kim-33@users.noreply.github.com>
2022-08-05 17:55:52 -0500

    progress?

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,29 +1,23 @@
-let knightPosition = [0, 0];
-let observer = null;
+let knightPosition = [0, 0]
+let observer = null
 
 function emitChange() {
-  observer(knightPosition);
+  observer(knightPosition)
 }
-
-// export function observe(receive) {
-//   const randPos = () => Math.floor(Math.random() * 8);
-//   setInterval(() => receive([randPos(), randPos()]), 500);
-// }
 
 export function observe(o) {
   if (observer) {
-    throw new Error("Multiple observers not implemented.");
+    throw new Error('Multiple observers not implemented.')
   }
 
-  observer = o;
-  emitChange();
+  observer = o
+  emitChange()
 }
 
 export function moveKnight(toX, toY) {
-  knightPosition = [toX, toY];
-  emitChange();
+  knightPosition = [toX, toY]
+  emitChange()
 }
-
 export function canMoveKnight(toX, toY) {
   const [x, y] = knightPosition;
   const dx = toX - x;
